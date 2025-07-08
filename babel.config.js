7c01dd789ee041c3c5d8fbcec23202ff5e6e6632
Usman Ghani <rajausman127official@gmail.com>
2025-07-08 19:59:45 +0500

    Added folder aliases, integrated react-hook-form, i18next, other libs

--- babel.config.js
+++ babel.config.js
@@ -5,4 +5,21 @@ module.exports = {
       plugins: ['react-native-paper/babel'],
     },
   },
+  plugins: [
+    '@babel/plugin-transform-export-namespace-from',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@components': './src/common/components',
+          '@features': './src/features',
+          '@theme': './src/common/theme',
+          '@navigation': './src/navigation',
+          '@localization': './src/localization',
+        },
+        extensions: ['.ts', '.tsx', '.js', '.json'],
+      },
+    ],
+  ],
 };
